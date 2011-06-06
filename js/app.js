09b83cc53729e844668c8a2b074b06eec454f31b
Anton Podviaznikov <podviaznikov@gmail.com>
2011-06-06 23:16:01 +0200

    added two colors. game simulation.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+var counter=0;
+var Point={
+    x:0,
+    y:0
+}
+var points=[];
 function draw() {
     var windowWidth=$(window).width()-30,
         windowHeight=$(window).height()-30,
@@ -22,7 +28,11 @@ function draw() {
        console.log(relX/40,relY/40);
        console.log(Math.floor(relX/40),Math.floor(relY/40));
        console.log(newX,newY);
-       drawCircle(ctx,newX,newY,20);
+       var newPoint=Object.create(Point);
+       newPoint.x=newX;
+       newPoint.y=newY;
+       points.push(newPoint);
+       drawCircle(ctx,newX,newY,10);
     });
 
 
@@ -41,11 +51,16 @@ function draw() {
 
     ctx.strokeStyle = '#eee';
     ctx.stroke();
-}
-function drawCircle(context,x,y,radius)
-{
+};
+function drawCircle(context,x,y,radius){
     context.beginPath();
-    context.fillStyle = "rgba(250, 0, 0, 0.5)";
+    if(counter%2===0){
+        context.fillStyle = 'rgba(250, 0, 0, 1)';
+    }else{
+        context.fillStyle = 'rgba(0, 250, 0, 1)';
+    }
     context.arc(x, y, radius, 0, Math.PI * 2, true);
     context.fill();
+    counter++;
+    console.log(points);
 };
